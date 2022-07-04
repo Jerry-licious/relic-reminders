8c47b86ec342719eb8fb212aca69d0070083a37d
Jerry <47512608+Jerry-Licious@users.noreply.github.com>
2022-07-03 23:33:23 -0400

    White Beast icon now only shows up during combat.

--- src/main/java/relicreminders/ui/nodes/relicicon/potionlist/WhiteBeastStatueIcon.kt
+++ src/main/java/relicreminders/ui/nodes/relicicon/potionlist/WhiteBeastStatueIcon.kt
@@ -4,6 +4,7 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon
 import com.megacrit.cardcrawl.helpers.PowerTip
 import com.megacrit.cardcrawl.relics.Sozu
 import com.megacrit.cardcrawl.relics.WhiteBeast
+import com.megacrit.cardcrawl.rooms.AbstractRoom
 import relicreminders.ui.nodes.relicicon.AbstractRelicIcon
 
 class WhiteBeastStatueIcon: AbstractRelicIcon(WhiteBeast(), 50f) {
@@ -11,5 +12,7 @@ class WhiteBeastStatueIcon: AbstractRelicIcon(WhiteBeast(), 50f) {
         tips.add(PowerTip(relic.name, relic.description))
     }
 
-    override fun display(): Boolean = super.display() && !AbstractDungeon.player.hasRelic(Sozu.ID)
+    override fun display(): Boolean = super.display() && !AbstractDungeon.player.hasRelic(Sozu.ID) &&
+            // Only show up in combat.
+            AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT
 }
