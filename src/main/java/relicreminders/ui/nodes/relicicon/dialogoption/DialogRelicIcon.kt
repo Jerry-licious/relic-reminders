faf03f5599aa3b5e1b8d58d45ff63bae796e98e0
Jerry <47512608+Jerry-Licious@users.noreply.github.com>
2022-07-05 14:54:03 -0400

    Abstract clarity.

--- src/main/java/relicreminders/ui/nodes/relicicon/dialogoption/DialogRelicIcon.kt
+++ src/main/java/relicreminders/ui/nodes/relicicon/dialogoption/DialogRelicIcon.kt
@@ -1,10 +1,8 @@
 package relicreminders.ui.nodes.relicicon.dialogoption
 
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon
 import com.megacrit.cardcrawl.relics.AbstractRelic
-import com.megacrit.cardcrawl.relics.Omamori
 import com.megacrit.cardcrawl.ui.buttons.LargeDialogOptionButton
 import relicreminders.ui.nodes.relicicon.AbstractRelicIcon
 
-open class DialogRelicIcon(relic: AbstractRelic, val dialogOption: LargeDialogOptionButton):
+abstract class DialogRelicIcon(relic: AbstractRelic, val dialogOption: LargeDialogOptionButton):
     AbstractRelicIcon(relic, 60f)
