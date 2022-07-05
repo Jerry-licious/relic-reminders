b6df4928476d95388fbd86ad134d1a0bf0e8ada9
Jerry <47512608+Jerry-Licious@users.noreply.github.com>
2022-07-05 14:54:25 -0400

    TextureRect now supports opacity.

--- src/main/java/relicreminders/ui/nodes/TextureRect.kt
+++ src/main/java/relicreminders/ui/nodes/TextureRect.kt
@@ -8,12 +8,15 @@ import relicreminders.ui.shape.Rectangle
 
 open class TextureRect(var texture: TextureRegion, shape: Rectangle): AbstractNode(shape) {
     var greyscale: Boolean = false
+    var renderColour: Color = Color.WHITE.cpy()
+    var opacity: Float = 1f
 
     override fun render(sb: SpriteBatch) {
         if (greyscale) ShaderHelper.setShader(sb, ShaderHelper.Shader.GRAYSCALE)
         val boundingBox = shape.boundingBox
 
-        sb.color = Color.WHITE
+        renderColour.a = opacity
+        sb.color = renderColour
         sb.draw(texture, boundingBox.position.x, boundingBox.position.y,
             boundingBox.size.x, boundingBox.size.y)
 
