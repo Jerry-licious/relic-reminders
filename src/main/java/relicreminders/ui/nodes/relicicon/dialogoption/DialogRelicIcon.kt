2b4a140dfa81600044d9a35ab7f1f6948d07a5ab
Jerry <47512608+Jerry-Licious@users.noreply.github.com>
2022-07-18 12:16:42 -0400

    Ceramic fish no longer shows up in the falling event.

--- src/main/java/relicreminders/ui/nodes/relicicon/dialogoption/DialogRelicIcon.kt
+++ src/main/java/relicreminders/ui/nodes/relicicon/dialogoption/DialogRelicIcon.kt
@@ -1,13 +1,17 @@
 package relicreminders.ui.nodes.relicicon.dialogoption
 
 import com.megacrit.cardcrawl.cards.AbstractCard
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon
+import com.megacrit.cardcrawl.events.beyond.Falling
 import com.megacrit.cardcrawl.relics.AbstractRelic
 import com.megacrit.cardcrawl.ui.buttons.LargeDialogOptionButton
 import relicreminders.ui.getPrivateFromClass
 import relicreminders.ui.nodes.relicicon.AbstractRelicIcon
 
 abstract class DialogRelicIcon(relic: AbstractRelic, val dialogOption: LargeDialogOptionButton):
-    AbstractRelicIcon(relic, 60f)
+    AbstractRelicIcon(relic, 60f) {
+    override fun display() = super.display() && AbstractDungeon.getCurrRoom().event !is Falling
+}
 
 val LargeDialogOptionButton.previewCard: AbstractCard?
     get() = getPrivateFromClass("cardToPreview", LargeDialogOptionButton::class.java)
